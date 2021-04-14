46729210502405eb22471eeb4743e60d1c692d0f
yeisonpx <yeisonpx@gmail.com>
2021-04-13 20:29:14 -0400

    Adding front-end changes

--- CurrencyMarket.FrontEnd/currency-market/src/api/currenciesService.js
+++ CurrencyMarket.FrontEnd/currency-market/src/api/currenciesService.js
@@ -1,7 +1,8 @@
 import $ from 'jquery';
 
 export default {
-    getCurrencies: async function (code) {           
+    getCurrencies: async function (code) {      
+        console.log(process.env);
        return await $.get(`https://localhost:44392/api/currencies/${code}`);
     }
 };
